faaba6c3532c77916c1cf36328f1f27579b7b3bf
Douglas Faria <douglas.faria@proxyerp.com.br>
2023-02-22 01:48:37 -0300

    feat: finalização do projeto de calculo de dolar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Comprar Dólar</h1>
+  <div class="container">
+    <div class="app-container">
+      <h2>Consultar Valores</h2>
+      <div class="input-values">
+        <span class="p-float-label money-inputs">
+          <primevue-inputtext id="input-dolar" type="text" v-model="real" />
+          <label for="input-dolar">Real</label>
+        </span>
+        <span class="p-float-label money-inputs">
+          <primevue-inputtext id="input-dolar" type="text" v-model="dollar" />
+          <label for="input-dolar">Dólar</label>
+        </span>
+      </div>
+      <div class="action-buttons">
+        <primevue-button label="Consultar" @click="calc" :disabled="!real || !dollar"/>
+        <primevue-button label="Limpar" @click="clear"/>
+      </div>
+    </div>
+    <div class="response-container" v-if="conversion">
+      <h2>Resultado</h2>
+      <div v-html="response"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      dollar: null,
+      real: null, 
+      conversion: null, 
+      response: ''
+    }
+  }, 
+  methods: {
+    clear: function() {
+      this.dollar = null;
+      this.real = null;
+      this.conversion = null;
+      this.response = ''
+    }, 
+    calc: function() {
+      this.conversion = (this.real / this.dollar).toFixed(2);
+      this.response = `<p>Com <strong>R$${this.real} reais</strong> é possível comprar <strong>$${this.conversion} dólares</strong> à <strong>$${this.dollar}</strong> cada.</p>`;
+    }
   }
 }
 </script>
@@ -19,8 +55,57 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+h1 {
+  text-align: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 10px;
+}
+
+h2 {
+  margin: 0;
+  text-align: center;
+}
+
+.app-container, 
+.response-container {
+  border: 2px solid #2c3e50;
+  border-radius: 15px;
+  padding: 40px;
+  width: 320px
+}
+
+.input-values {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.money-inputs,
+.action-buttons {
+  margin-top: 2rem;
+}
+
+.action-buttons {
+  display: flex;
+  justify-content: space-around;
+}
+
+.action-buttons > button {
+  width: 100px;
+}
+
+p {
+  text-align: center;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css 
+
+const app = createApp(App);
+
+app.use(PrimeVue); 
+app.component('primevue-inputtext', InputText);
+app.component('primevue-button', Button);
+
+app.mount('#app')
